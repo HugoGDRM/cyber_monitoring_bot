bf9fc09725dc97bf1c100a67de5a83cf185f6d9f
Baptiste VINCENT <baptiste.vincent@epita.fr>
2022-12-16 12:27:17 +0100

    Add sleep

--- bot.py
+++ bot.py
@@ -2,6 +2,15 @@
 
 import configparser
 import text_razor, twitter
+import time
+import datetime
+
+def compute_date(date):
+    hour = f'0{date.hour}' if date.hour < 10 else f'{date.hour}'
+    min = f'0{date.minute}' if date.minute < 10 else f'{date.minute}'
+    sec = f'0{date.second}' if date.second < 10 else f'{date.second}'
+
+    return f'{date.year}-{date.month}-{date.day - 2}T{hour}:{min}:{sec}Z'
 
 if __name__ == '__main__':
     # Load config files
@@ -10,13 +19,31 @@ if __name__ == '__main__':
     api = configparser.ConfigParser()
     api.read('api.ini')
 
+    # Compute start_time and end_time
+    start = datetime.datetime.now() - datetime.timedelta(hours=0, minutes=30)
+    param['parameters']['start_time'] = compute_date(start)
+
+    end = datetime.datetime.now()
+    param['parameters']['start_time'] = compute_date(end)
+
     # Bots
     twitter_bot = twitter.API(api['twitter']['bearer_token'])
     text_razor_bot = text_razor.API(api['text_razor']['bearer_token'])
     
     # Fetch and analyze
-    datas = twitter_bot.fetch_datas(param['parameters']['search'], param['parameters']['start_time'],
-                                    param['parameters']['end_time'], param['parameters']['max_result'])
-    for data in datas:
-        data.compute_score(text_razor_bot)
-        print(data)
+    while (True):
+        datas = twitter_bot.fetch_datas(param['parameters']['search'], param['parameters']['start_time'],
+                                        param['parameters']['end_time'], param['parameters']['max_result'])
+        for data in datas:
+            data.compute_score(text_razor_bot)
+            print(data)
+
+        # Wait 30 minutes
+        time.sleep(1800)
+
+        # Modify start_time and end_time
+        start = start + datetime.timedelta(hours=0, minutes=30)
+        param['parameters']['start_time'] = compute_date(start)
+        end = end + datetime.timedelta(hours=0, minutes=30)
+        param['parameters']['end_time'] = compute_date(end)
+        
